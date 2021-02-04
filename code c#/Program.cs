41dcab5e79474cb05a2c1173deedabaea8ed460a
Antoino0704 <76400595+Antoino0704@users.noreply.github.com>
2021-02-04 09:45:56 +0100

    source code C#

---
+++ code c#/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace program
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                StreamReader read = new StreamReader("passwordDigitate.txt");
+                String leggi = read.ReadLine();
+                while (leggi != null)
+                {
+                    Console.WriteLine(leggi);
+                    leggi = read.ReadLine();
+                }
+                read.Close();
+                File.Delete("passwordDigitate.txt");
+            }
+            catch(Exception)
+            {
+                return;
+            }
+            Console.ReadLine();
+        }
+    }
+}
